4b5e23b4e230330b3040551d58f1baa612be63a0
alkis <miketheud@gmail.coom>
2019-05-17 22:22:17 +0300

    jacob1

--- Owner.java
+++ Owner.java
@@ -18,24 +18,43 @@ public class Owner extends User{
     public ArrayList<Reservation> getReservationId() {
         return reservationId;
     }
-    public void manageResevation(Studio studioId,User userId, Reservation reservationId,Room roomId, boolean pending, boolean accept){
+    public void confirmResevation(Studio studio,User user, Reservation reservation,Room room, boolean pending, boolean accept){
         for (Studio i:this.getPartnerStudios()){
-            if(i.getStudioId()==studioId){
+            if(i.getStudioId()==studio){
                 for (Reservation j:i.getReservations()){
-                    if(j.getReservationId() == reservationId){
-                        if(j.isConfirmed() == pending && accept==true){
-                            //if(accept==true )
-                                // j.setConfirmed(true)
-                            //else diagrafo th krathsh
-                            //i.getReservations().remove(reservationId) remove ekei pou einai to rese toso
-
-                            Reservation reservation = new Reservation(Studio studioId,User userId, Reservation reservationId,Room roomId, boolean pending);
-                        }else if()
+                    if(j.getReservationId() == reservation){
+                        if(j.isConfirmed() == pending) {
+                            if (accept == true) {
+                                j.setConfirmed(true);
+                                System.out.println("The reservation of the user" + user + " is confirmed");
+                            } else {
+                                i.getReservations().remove(reservation);
+                                System.out.println("The reservation of the user" + user + " is declined");
+                            }
+                            //ή μπορώ να χω 2 συναρτήσεις , που η μια να κάνει κονφιρμ πχ και η άλλη να κάνει αλλαγές ( ίδιο όνομα , διαφορετικά ορίσματα)(sId,us9d,res9d,pend) , mr(5orismata) diaforetikh
+                            //ή να καλεί την ίδια συνάρτηση και να βάλεις να έχει αν θες να πειράξεις μονο το pending και όχι πχ μουσικό όργανο
+                        }   // , τότε να έχει default τιμές τα ορίσματα πχ null , και τσεκάρεις να είναι null με if και αν ναι τότε δεν αλλάζεις πχ (int musinsid:default null)
                     }
                 }
             }
         }
     }
+
+    public void manageReservationRoom(Studio studio, Reservation reservation,Room room, boolean acceptChanges){
+        for (Studio i:this.getPartnerStudios()) {
+            if (i.getStudioId() == studio) {
+                for (Reservation j : i.getReservations()) {
+                    if (j.getReservationId() == reservation) {
+                        if (j.getRoomId() == room) {
+                            j.setRoomId(room);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
 
     public void makeOwnOffer(String type, int resNum, int free , int studioId, boolean state) {
             for(Studio i:this.getPartnerStudios()){
