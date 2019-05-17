fff4287d64da25db8679ce1eb39823b16230b562
alkis <miketheud@gmail.coom>
2019-05-17 20:51:52 +0300

    v0.1iason

--- Owner.java
+++ Owner.java
@@ -4,6 +4,7 @@ public class Owner extends User{
 
     private int ownerId;
     private ArrayList<Studio> partnerStudios;
+    private ArrayList<Reservation> reservationId;
 
     public Owner(String name, String lastName, String phone, String email, String password, int ownerId) {
         super(name, lastName, phone, email, password);
@@ -12,6 +13,23 @@ public class Owner extends User{
 
     public ArrayList<Studio> getPartnerStudios() {
         return partnerStudios;
+    }
+
+    public ArrayList<Reservation> getReservationId() {
+        return reservationId;
+    }
+    public void manageResevation(Studio studioId,User userId, Reservation reservationId,Room roomId, boolean pending, boolean accept){
+        for (Studio i:this.getPartnerStudios()){
+            if(i.getStudioId()==studioId){
+                for (Reservation j:this.getReservationId()){
+                    if(j.getReservationId() == reservationId){
+                        if(j.isConfirmed() == pending && accept==true){
+                            Reservation reservation = new Reservation(Studio studioId,User userId, Reservation reservationId,Room roomId, boolean pending);
+                        }else if()
+                    }
+                }
+            }
+        }
     }
 
     public void makeOwnOffer(String type, int resNum, int free , int studioId, boolean state) {
@@ -26,7 +44,7 @@ public class Owner extends User{
         for(Studio i:this.getPartnerStudios()){
             if(i.getStudioId()==studioId){
                 for(int j=0;j<i.getOffers().size();j++){
-                    if (i.getOffers().get(j).getOfferId()==offerId) {
+                    if (i.getOffers().get(j).getOfferId()==offerId){
                         i.getOffers().get(j).setState(state);
                         return;
                     }
